c0692b4c33457c6c1f1d44250bd01a74bf73468e
Ferdinand Bachmann <theferdi265@gmail.com>
2023-09-03 16:26:43 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "vsmodupdater",
+    description = "a mod updater for VintageStory",
+    version = "0.1.0",
+    author = "Ferdinand Bachmann",
+    author_email = "theferdi265@gmail.com",
+    packages = [
+        "vsmodupdater",
+    ],
+    entry_points = {
+        "console_scripts": [
+            "vsmodupdater=vsmodupdater.cli:main",
+        ]
+    },
+    package_data = {
+        "vsmodupdater": ["py.typed"]
+    },
+    python_requires = ">=3.10",
+    install_requires = requirements
+)
