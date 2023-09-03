cb2d812f9f86d2314122a495ef2d11b5831bd684
Ferdinand Bachmann <theferdi265@gmail.com>
2023-09-03 22:20:28 +0200

    setup: bump version to 0.3.0, lower python_requires to 3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", "r") as f:
 setup(
     name = "vsmodupdater",
     description = "a mod updater for VintageStory",
-    version = "0.2.0",
+    version = "0.3.0",
     author = "Ferdinand Bachmann",
     author_email = "theferdi265@gmail.com",
     packages = [
@@ -20,6 +20,6 @@ setup(
     package_data = {
         "vsmodupdater": ["py.typed"]
     },
-    python_requires = ">=3.10",
+    python_requires = ">=3.7",
     install_requires = requirements
 )
